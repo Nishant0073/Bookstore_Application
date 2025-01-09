2a12375725d130a12e4877e4f032dac85b270ce3
Nishant Shingate <nishantks121@gmail.com>
2025-01-09 22:58:53 +0530

    Added Connection string and configured it.

--- Bookstore_Application/Program.cs
+++ Bookstore_Application/Program.cs
@@ -1,5 +1,10 @@
+using Bookstore_Application.Data;
+using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
-
+string connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ??
+                          throw new InvalidOperationException();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
 // Add services to the container.
 builder.Services.AddControllers();
 
