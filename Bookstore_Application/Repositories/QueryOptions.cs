3cf8e654776fb10a016beba545f33df2bce3ec92
Nishant Shingate <nishantks121@gmail.com>
2025-01-10 22:17:53 +0530

    Added QueryOptions.cs

---
+++ Bookstore_Application/Repositories/QueryOptions.cs
@@ -0,0 +1,18 @@
+using System.Linq.Expressions;
+
+namespace Bookstore_Application.Repositories;
+
+public class QueryOptions<T> where T : class
+{ 
+    public Expression<Func<T, Object>> OrderBy { get; set; } = null!;
+    public Expression<Func<T,bool>> Where { get; set; } = null!;
+    private string[] _includes = Array.Empty<string>();  
+    public string Includes
+    {
+        set => _includes = value.Replace(" ","").Split(',');
+    }
+    public string[] GetIncludes() => _includes;  
+    public bool HasWhere => Where != null;
+    public bool HasOrderBy => OrderBy != null;  
+    
+}
