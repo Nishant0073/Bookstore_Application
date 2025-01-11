23d6c1ddaeba1265d45a7e26de0e60e2f1fbd505
Nishant Shingate <nishantks121@gmail.com>
2025-01-11 12:21:26 +0530

    Added logger and registered Repo

--- Bookstore_Application/Program.cs
+++ Bookstore_Application/Program.cs
@@ -1,4 +1,6 @@
 using Bookstore_Application.Data;
+using Bookstore_Application.Models;
+using Bookstore_Application.Repositories;
 using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 string connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ??
@@ -7,6 +9,11 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
 // Add services to the container.
 builder.Services.AddControllers();
+
+//Adding Loggers for Repository
+builder.Services.AddScoped<IRepository<Category>, CategoryRepository>();
+builder.Logging.AddConsole();
+builder.Logging.AddDebug();
 
 var app = builder.Build();
 
