be7bbe43eff1173d5967fc1817b5111da4e96a0a
Nishant Shingate <nishantks121@gmail.com>
2025-01-14 22:32:35 +0530

    Added OrderStatus

--- Bookstore_Application/Models/Order.cs
+++ Bookstore_Application/Models/Order.cs
@@ -7,7 +7,7 @@ public class Order
     public string UserId { get; set; }
     public DateTime OrderDate { get; set; }
     public double TotalPrice { get; set; }
-    
+    public string OrderStatus { get; set; } = "Pending";
     
     public ICollection<OrderItem> OrderItems { get; set; }
     
