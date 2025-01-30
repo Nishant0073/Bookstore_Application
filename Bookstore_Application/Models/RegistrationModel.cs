96cf4c93081381bfb1e821954e46adf046a2a20d
Nishant Shingate <nishantks121@gmail.com>
2025-01-30 08:23:16 +0530

    Added validation

--- Bookstore_Application/Models/RegistrationModel.cs
+++ Bookstore_Application/Models/RegistrationModel.cs
@@ -5,7 +5,9 @@ namespace Bookstore_Application.Models;
 public class RegistrationModel
 {
     [Required]
+    [EmailAddress]
     public string Email { get; set; }
     [Required]
+    [DataType(DataType.Password)]
     public string Password { get; set; }
 }
